85cdc5a2b7e85c998dbceb8168440d7365d2d1ea
VicenteMonfort <vmonfort@florida-uni.es>
2020-12-02 12:55:43 +0100

    Subiendo js

---
+++ Tema2Tarea1.js
@@ -0,0 +1,23 @@
+let a = 'Are you ready?';
+let b = a.length-1;
+let c=[];
+let d=[];
+let j=0;
+
+for (i=0;i<=b;i++){
+    c[i]=a.slice(i,i+1);
+}
+
+for (i=c.length-1;i>=0;i--){
+    d[j]=c[i];
+    j++;
+}
+
+for (i=0;i<=d.length-2;i++){
+    e=d[i];
+    d[i]=d[i+1];
+    d[i+1]=e;
+    i++;
+}
+
+
